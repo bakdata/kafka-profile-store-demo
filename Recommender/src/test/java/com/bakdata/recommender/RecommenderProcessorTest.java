dc4e8e582dd9accc9b8c088ab1fa007f9178513f
torbsto <torben.meyer2@student.hpi.de>
2019-08-06 09:59:58 +0200

    Add test for multiple inputs

--- Recommender/src/test/java/com/bakdata/recommender/RecommenderProcessorTest.java
+++ Recommender/src/test/java/com/bakdata/recommender/RecommenderProcessorTest.java
@@ -1,9 +1,11 @@
 package com.bakdata.recommender;
 
+import com.bakdata.fluent_kafka_streams_tests.TestInput;
 import com.bakdata.fluent_kafka_streams_tests.junit5.TestTopologyExtension;
 import com.bakdata.recommender.avro.ListeningEvent;
 import com.bakdata.recommender.graph.KeyValueGraph;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumMap;
 import org.junit.Test;
@@ -52,6 +54,38 @@ class RecommenderProcessorTest {
                 graphMap.get(RecommendationType.ALBUM).getLeftNodeNeighbors(1));
         Assertions.assertEquals(Collections.singletonList(1L),
                 graphMap.get(RecommendationType.ALBUM).getRightNodeNeighbors(4));
+    }
+
+    @Test
+    public void testMultipleInputs() {
+        long[] users = {1, 5, 1, 6, 1};
+        long[] artists = {2, 3, 4, 5, 2};
+        long[] album = {3, 3, 4, 5, 6};
+        long[] track = {4, 8, 2, 8, 7};
+
+        TestInput<String, ListeningEvent> testInput = this.testTopology.input();
+
+        for (int i = 0; i < users.length; i++) {
+            testInput.add(new ListeningEvent(users[i], artists[i], album[i], track[i], Instant.now()));
+        }
+
+        EnumMap<RecommendationType, KeyValueGraph> graphMap = this.getGraphMap();
+
+        Assertions.assertEquals(Arrays.asList(3L, 4L, 6L),
+                graphMap.get(RecommendationType.ALBUM).getLeftNodeNeighbors(1));
+        Assertions.assertEquals(Collections.singletonList(1L),
+                graphMap.get(RecommendationType.ALBUM).getRightNodeNeighbors(4));
+
+        Assertions.assertEquals(Arrays.asList(2L, 4L, 2L),
+                graphMap.get(RecommendationType.ARTIST).getLeftNodeNeighbors(1));
+        Assertions.assertEquals(Arrays.asList(1L, 1L),
+                graphMap.get(RecommendationType.ARTIST).getRightNodeNeighbors(2));
+
+        Assertions.assertEquals(Arrays.asList(4L, 2L, 7L),
+                graphMap.get(RecommendationType.TRACK).getLeftNodeNeighbors(1));
+        Assertions.assertEquals(Arrays.asList(5L, 6L),
+                graphMap.get(RecommendationType.TRACK).getRightNodeNeighbors(8));
+
     }
 
     private EnumMap<RecommendationType, KeyValueGraph> getGraphMap() {
