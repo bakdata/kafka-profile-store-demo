08a13eb2f84e8b793c1e6d6bc3b626ef6b27f5af
torbsto <torben.meyer2@student.hpi.de>
2019-08-28 10:30:50 +0200

    Add comment

--- profile-store-rest/src/main/java/com/bakdata/profilestore/rest/UserPartitioner.java
+++ profile-store-rest/src/main/java/com/bakdata/profilestore/rest/UserPartitioner.java
@@ -3,6 +3,10 @@ package com.bakdata.profilestore.rest;
 import java.nio.ByteBuffer;
 import org.apache.kafka.common.utils.Utils;
 
+/**
+ * UserPartitioner implements Kafka's DefaultPartitioner logic to calculate the partition for a user id given the number
+ * of partitions
+ */
 public class UserPartitioner {
     public static int calculatePartition(final long userId, final int numPartitions) {
         final byte[] keyBytes = keyToByteArray(userId);
