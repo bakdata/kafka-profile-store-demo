b35d9922cb7a5a771414dea1f33bf1bd39fa6a32
alpreu <11444089+alpreu@users.noreply.github.com>
2019-09-24 13:58:14 +0200

    outputting user ID file

--- profile-store-generator/preprocess_data.sh
+++ profile-store-generator/preprocess_data.sh
@@ -18,3 +18,6 @@ perl -lane 'BEGIN{open(A,"artist_ids_at_least_100_events.txt"); while(<A>){chomp
 # Get all tracks where the artist ID is included in the filtered ID file
 perl -lane 'BEGIN{open(A,"artist_ids_at_least_100_events.txt"); while(<A>){chomp; $k{$_}++}}
             print $_ if defined($k{$F[2]}); ' LFM-1b_tracks.txt > LFM-1b_tracks-filtered.txt
+
+# Get all user IDs contained in the filtered down listening events file
+cut -f-1 LFM-1b_LEs-filtered.txt | sort -u > user_ids_filtered.txt
